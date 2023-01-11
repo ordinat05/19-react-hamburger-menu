45edafddbd26bc65f66f53aeb4d011eea3b8adf5
sebahattin <sebkucuk05@gmail.com>
2023-01-11 17:26:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* App.jsx Sayfasından Selamlar. */}
+
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
